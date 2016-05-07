40d2859799b8d555c0868bba853728b268ee273c
AdrianDC <radian.dc@gmail.com>
2016-05-07 02:49:55 +0200

    msm8916-common: Improve Camera HAL wrapper for SensorService init
     * Update based upon original commit in oppo_msm8939-common
        I1fcf77146317c31407a0efc16429066555186643 by Danny Baumann
    
    Change-Id: I159c390f24e553cc738b9cd407f49cc1d9c23a78

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,10 +1,7 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := \
-    system/media/camera/include \
-    frameworks/native/include/media/openmax
+LOCAL_C_INCLUDES := system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
@@ -12,10 +9,8 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils libcutils libgui libsensor
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
-endif
