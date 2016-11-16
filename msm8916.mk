8674d3fdf27e77af5b2b23606d477f2fdbc62e42
Adrian DC <radian.dc@gmail.com>
2016-11-16 13:02:04 +0100

    msm8916-common: Increase ART heap growth limit to 192MB
     * Snapshot values exported from frameworks/native
        of the build/phone-xhdpi-1024-dalvik-heap.mk file:
    
        "Fix issue #7257947: Need to fix heap limits on Mako"
        Change-Id: Id8014b19195b8740bccf000ab66c1b599ba3a1a0
    
    From: Adrian DC <radian.dc@gmail.com>
    Date: Wed, 16 Nov 2016 13:02:04 +0100
    
        native: Increase ART heap limit to 192MB for 1024MB RAM devices
    
         * Needed for Google SetupWizard to start
    
         * Without this, SetupWizard is compiled by ART on boot
           and runs out of memory (heap) while running, resulting
           in the SetupWizard crashing and blocking boot
    
        Change-Id: Ic5b8caf4352f6d104af9121a09a01adad1c5bcc3
    
    Change-Id: Iee6766c3a912c0b841faa41c3d0776e87478817d
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -164,6 +164,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Dalvik properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 # Data configuration files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
