ce40c8d11f8f693ddb22cd7d59757a4e1d618172
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-02 16:03:26 -0600

    msm8916: Initialise base repository structure

--- msm8916.mk
+++ msm8916.mk
@@ -14,8 +14,231 @@
 # limitations under the License.
 #
 
+# Inherit from common
+$(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
+
 LOCAL_PATH := device/samsung/msm8916-common
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# ANT+
+PRODUCT_PACKAGES += \
+	AntHalService \
+	antradio_app \
+	com.dsi.ant.antradio_library \
+	libantradio
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.primary.msm8916 \
+	audio.primary.default \
+	audio_policy.msm8916 \
+	audio.r_submix.default \
+	audio.tms.default \
+	audio.usb.default \
+	audiod \
+	libaudio-resampler \
+	libaudioroute \
+	libaudioutils \
+	libaudiopolicymanager \
+	libqcompostprocbundle \
+	libqcomvisualizer \
+	libqcomvoiceprocessing \
+	libqcmediaplayer \
+	libtinycompress \
+	tinymix \
+	tinyplay \
+	tinycap \
+	tinypcminfo
+
+# Camera
+PRODUCT_PACKAGES += \
+	libmm-qcamera \
+	camera.msm8916 \
+	Snap
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	camera2.portability.force_api=1
+
+# GPS
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
+	$(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+	$(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
+	$(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.gps.qc_nlp_in_use=1
+
+# BoringSSL Hacks
+PRODUCT_PACKAGES += \
+	libboringssl-compat
+
+# Libtime
+PRODUCT_PACKAGES += \
+	libtime_genoff
+
+# Boot jars
+PRODUCT_BOOT_JARS += \
+	tcmiface
+
+# Connectivity Engine support
+PRODUCT_PACKAGES += \
+	libcnefeatureconfig
+
+# Location, WiDi
+PRODUCT_PACKAGES += \
+	com.android.location.provider \
+	com.android.location.provider.xml \
+	com.android.media.remotedisplay \
+	com.android.media.remotedisplay.xml
+
+# Display
+PRODUCT_PACKAGES += \
+	copybit.msm8916 \
+	gralloc.msm8916 \
+	hwcomposer.msm8916 \
+	libtinyxml \
+	libtinyxml2 \
+	memtrack.msm8916
+
+# Ebtables
+PRODUCT_PACKAGES += \
+	ebtables \
+	ethertypes \
+	libebtc
+
+# libxml2
+PRODUCT_PACKAGES += \
+	libxml2
+
+# Keystore
+PRODUCT_PACKAGES += \
+	keystore.msm8916
+
+# libhealthd.qcom
+PRODUCT_PACKAGES += \
+	libhealthd.qcom \
+	libhealthd
+
+# Power HAL
+PRODUCT_PACKAGES += \
+	power.qcom
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.msm8916
+
+# Default Property Overrides
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp \
+	persist.radio.apm_sim_not_pwdn=1 \
+	persist.cne.feature=0 \
+	ro.debuggable=1 \
+	persist.service.adb.enable=1
+
+# Sensors
+PRODUCT_PACKAGES += \
+	sensors.default
+
+# Macloader
+PRODUCT_PACKAGES += \
+	macloader
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.heapgrowthlimit=128m \
+	ro.security.icd.flagmode=single \
+	ro.vendor.extension_library=libqti-perfd-client.so \
+	persist.loc.nlp_name=com.qualcomm.location
+
+# Media configurations
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+# Misc
+PRODUCT_PACKAGES += \
+	curl \
+	libbson \
+	libcurl \
+	javax.btobex \
+	tcpdump \
+	libkeyutils \
+	libjpega \
+	libexifa \
+	libstlport \
+	datatop \
+	sockev \
+	librmnetctl
+
+# OMX
+PRODUCT_PACKAGES += \
+	libextmedia_jni \
+	libdashplayer \
+	libdivxdrmdecrypt \
+	libmm-omxcore \
+	libOmxAacEnc \
+	libOmxAmrEnc \
+	libOmxCore \
+	libOmxEvrcEnc \
+	libOmxQcelp13Enc \
+	libOmxSwVencMpeg4 \
+	libOmxVdec \
+	libOmxVdecHevc \
+	libOmxVenc \
+	libOmxVidEnc \
+	libOmxVdpp \
+	libstagefrighthw
+
+# Power configuration
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
+# Wifi configuration files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/wifi/cred.conf:system/etc/wifi/cred.conf \
+	$(LOCAL_PATH)/configs/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
+	$(LOCAL_PATH)/configs/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+	$(LOCAL_PATH)/configs/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
+	$(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+	$(LOCAL_PATH)/configs/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+	$(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+	$(LOCAL_PATH)/configs/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+	$(LOCAL_PATH)/configs/wifi/sec_config:system/etc/sec_config \
+	$(LOCAL_PATH)/configs/wifi/dsi_config.xml:system/etc/data/dsi_config.xml \
+	$(LOCAL_PATH)/configs/wifi/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+	$(LOCAL_PATH)/configs/wifi/qmi_config.xml:system/etc/data/qmi_config.xml
+
+# Wifi
+PRODUCT_PACKAGES += \
+	hostapd \
+	iwconfig \
+	hostapd_cli \
+	libQWiFiSoftApCfg \
+	libqsap_sdk \
+	libwpa_client \
+	libwcnss_qmi \
+	wcnss_service \
+	wpa_supplicant
