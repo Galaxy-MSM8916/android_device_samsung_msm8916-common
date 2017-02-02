abcb5ccf85207a01f6885f21552b786f82a002e5
deadman96385 <seanhoyt963@gmail.com>
2017-02-02 16:54:35 -0600

    Add kernel flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,16 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+
 # ART
 WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
