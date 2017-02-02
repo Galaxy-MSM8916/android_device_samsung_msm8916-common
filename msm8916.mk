2c44c4c7115885ff53f021780694b23c83cbe696
deadman96385 <seanhoyt963@gmail.com>
2017-02-02 16:35:43 -0600

    Add ramdisk files

--- msm8916.mk
+++ msm8916.mk
@@ -29,6 +29,28 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	camera2.portability.force_api=1
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+	fstab.qcom \
+	init.carrier.rc \
+	init.class_main.sh \
+	init.ksm.sh \
+	init.mdm.sh \
+	init.qcom.audio.sh \
+	init.qcom.bt.sh \
+	init.qcom.uicc.sh \
+	init.qcom.wifi.sh \
+	init.qcom.post_boot.sh \
+	init.qcom.class_core.sh \
+	init.qcom.early_boot.sh \
+	init.qcom.syspart_fixup.sh \
+	init.qcom.usb.rc \
+	init.qcom.usb.sh \
+	init.qcom.rc \
+	init.qcom.fm.sh \
+	init.qcom.sh \
+	ueventd.qcom.rc
+
 # Wifi configuration files
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/wifi/cred.conf:system/etc/wifi/cred.conf \
