1f39d2c64f874180459d06cc1bd348fbf72fb8e4
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-16 23:59:13 -0500

    msm8916: Remove init.carrier.rc
     Carrier init scripts are ususally device specific, so allow the
     individual devices to install their own init.carrier.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -21,16 +21,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
-### init.carrier.rc
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.carrier.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-######################
 ### init.class_main.sh
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_main.sh
