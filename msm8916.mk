dc95cf40c5399b2f2429154632745e8cceeb031f
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-18 02:13:38 -0500

    msm8916: Allow camera app to be specified in device subtrees

--- msm8916.mk
+++ msm8916.mk
@@ -70,8 +70,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
 	libmm-qcamera \
-	camera.msm8916 \
-	Camera2
+	camera.msm8916
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	camera2.portability.force_api=1
