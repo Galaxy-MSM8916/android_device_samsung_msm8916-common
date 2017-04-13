d1ad6ebfb5db19e95bf42134a22d78db9abff839
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-13 04:02:41 -0400

    init: Link telephony database at boot
    msm8916: Update ril database link script
    
    * Rename file from init.link_rb.sh -> init.link_ril_db.sh
    * Update file linking logic: Loop through files in old location
      and link files individually.
    
     Date:      Thu Apr 13 04:02:41 2017 -0400
     interactive rebase in progress; onto 148f791
        pick 5127790 init: Link telephony database at boot
        squash 6ca00ff msm8916: Update ril database link script

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -180,6 +180,14 @@ LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT)/etc
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.link_ril_db.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 ######################
 ### twrp.fstab
 include $(CLEAR_VARS)
