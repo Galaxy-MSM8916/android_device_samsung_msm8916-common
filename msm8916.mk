7877e78c145fceb24fad65b34a62d00716967ef1
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-26 21:55:11 -0400

    configs: Add media configuration files
    * Import from J500HXXU2BQC4_J500HOXE2BQC2_CAU

--- msm8916.mk
+++ msm8916.mk
@@ -156,9 +156,14 @@ PRODUCT_COPY_FILES += \
 
 # Media configurations
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_sec_primary.xml:system/etc/media_codecs_sec_primary.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_sec_secondary.xml:system/etc/media_codecs_sec_secondary.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
