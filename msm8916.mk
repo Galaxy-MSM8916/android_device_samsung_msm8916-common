74658fb8016d3441b93053433e548aa06baa19c6
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-28 11:24:49 -0400

    msm8916: Fix media configuration file paths

--- msm8916.mk
+++ msm8916.mk
@@ -55,11 +55,11 @@ PRODUCT_COPY_FILES += \
 
 # Media configurations
 PRODUCT_COPY_FILES += \
-	configs/media/media_codecs.xml:ssytem/etc/media/media_codecs.xml \
-	configs/media/media_codecs_performance.xml:system/etc/media/media_codecs_performance.xml \
-	configs/media/media_codecs_sec_primary.xml:system/etc/media/media_codecs_sec_primary.xml \
-	configs/media/media_codecs_sec_secondary.xml:system/etc/media/media_codecs_sec_secondary.xml \
-	configs/media/media_profiles.xml:system/etc/media/media_profiles.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs.xml:ssytem/etc/media/media_codecs.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media/media_codecs_performance.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_sec_primary.xml:system/etc/media/media_codecs_sec_primary.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_sec_secondary.xml:system/etc/media/media_codecs_sec_secondary.xml \
+	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
