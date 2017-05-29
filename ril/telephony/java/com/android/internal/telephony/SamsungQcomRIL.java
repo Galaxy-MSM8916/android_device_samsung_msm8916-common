c2e14444b9c5198327bcb1e50186facd49b4fd4c
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-29 15:16:43 -0400

    ril: Update processUnsolicited function signatures
    * Function signature changed in android 7.1

--- ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
@@ -391,7 +391,7 @@ public class SamsungQcomRIL extends RIL {
 
     @Override
     protected void
-    processUnsolicited (Parcel p) {
+    processUnsolicited (Parcel p, int type) {
         Object ret;
         int dataPosition = p.dataPosition();
         int response = p.readInt();
@@ -407,7 +407,7 @@ public class SamsungQcomRIL extends RIL {
             p.writeInt(newResponse);
         }
         p.setDataPosition(dataPosition);
-        super.processUnsolicited(p);
+        super.processUnsolicited(p, type);
     }
 
     @Override
@@ -427,7 +427,7 @@ public class SamsungQcomRIL extends RIL {
 
     @Override
     protected RILRequest
-    processSolicited (Parcel p) {
+    processSolicited (Parcel p, int type) {
         int serial, error;
         boolean found = false;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
@@ -459,7 +459,7 @@ public class SamsungQcomRIL extends RIL {
             /* Nothing we care about, go up */
             p.setDataPosition(dataPosition);
             // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p);
+            return super.processSolicited(p, type);
         }
         rr = findAndRemoveRequestFromList(serial);
         if (rr == null) {
