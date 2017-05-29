792bca3cefb4e86f6f36795a80fa4a6ef72444c7
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-29 14:33:48 -0400

    configs: Remove audio configuration files
    * The configs are already provided as part of the qcom audio stack.

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,8 +1,3 @@
-# Audio configurations
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-	$(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
-
 # Audio
 PRODUCT_PACKAGES += \
 	audio.a2dp.default \
