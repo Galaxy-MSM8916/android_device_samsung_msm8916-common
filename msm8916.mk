f29152b5b1107c714eebbf26d032dad92f023d74
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-28 20:29:52 -0400

    configs: Move media_profiles config to device-commons
    * Configuration is hardware dependent, so one file will not fit
      all msm8916 devices.

--- msm8916.mk
+++ msm8916.mk
@@ -57,7 +57,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media/media_codecs.xml \
 	$(LOCAL_PATH)/configs/media/media_codecs_performance.xml:system/etc/media/media_codecs_performance.xml \
-	$(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media/media_profiles.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
