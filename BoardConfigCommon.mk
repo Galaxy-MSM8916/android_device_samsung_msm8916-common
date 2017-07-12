9d7979297c5ce09abb557a899e16532ad6f1ac69
padarshr <padarshr@codeaurora.org>
2017-07-12 17:32:34 +0530

    msm8916-common: Add device compatibility matrix.
    Add device compatibility matrix to common project and
    targets can point the build variable DEVICE_MATRIX_FILE to
    this file to build device compatibility matrix.
    
    Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,6 +145,9 @@ USE_DEVICE_SPECIFIC_GPS := true
 # Healthd
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 
+# HIDL
+DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8916
