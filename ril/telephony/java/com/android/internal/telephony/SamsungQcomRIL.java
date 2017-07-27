97377fdfccf81c10d1bcb8491eb8f715d325ac62
vlw <vlwwwwww@gmail.com>
2017-07-27 16:23:04 +0300

    msm8916-common: ril: Add call end reason support
    * If call is ended, ril should get reason of call end
      and show correct info in dialer.
    
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
@@ -518,4 +518,22 @@ public class SamsungQcomRIL extends RIL {
         }
         return response;
     }
+
+    protected Object
+    responseFailCause(Parcel p) {
+        int numInts;
+        int response[];
+
+        numInts = p.readInt();
+        response = new int[numInts];
+        for (int i = 0 ; i < numInts ; i++) {
+            response[i] = p.readInt();
+        }
+        LastCallFailCause failCause = new LastCallFailCause();
+        failCause.causeCode = response[0];
+        if (p.dataAvail() > 0) {
+          failCause.vendorCause = p.readString();
+        }
+        return failCause;
+    }
 }
