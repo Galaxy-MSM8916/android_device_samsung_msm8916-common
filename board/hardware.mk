985ce22e1f8874abafa6fe43b2d711661925f3cb
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-01 03:00:30 -0400

    board: hardware: Fix hardware class precedence
     * The msm8916-common classes should have precedence
       in the event that classes with the same name exist in
       both msm8916-common and hardware/samsung or hardware/cyanogen.
    
    Change-Id: I56650f58a2a2cf37528ad4883b4d331e48f8e056

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,6 +1,6 @@
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS :=	$(PLATFORM_PATH)/cmhw
 BOARD_HARDWARE_CLASS +=	\
 	hardware/cyanogen/cmhw \
-	hardware/samsung/cmhw
+	hardware/samsung/cmhw \
+	$(PLATFORM_PATH)/cmhw
