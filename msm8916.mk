02199841b43a2c11ec02b5df1f09de322fe38e15
zljing <zljing@codeaurora.org>
2017-08-02 14:44:58 +0800

    msm8916-common: prop: Shorten wait time to optimize shutdown time
    Mountservice will check if there exist processes occupy files. Launch
    many 3rd apps and set in background will cause many processes occupy
    files, which will cause mountservice spend 10~20s to unmount(sometimes
     also unmount fail with 20s time out). If there is no process occupy
    files, mountservice only need about 500ms. So shorten wait time from
    20s to 500ms, which will save 10~20s for shutdown time, total shutdown
    time is in 3s.
    
    CRs-Fixed: 2038121
    
    Change-Id: I4da7bbf4c86d05add7f1b1e328f70796f3bb8ee8

--- msm8916.mk
+++ msm8916.mk
@@ -132,6 +132,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.bluetooth.soc=pronto \
     vendor.qcom.bluetooth.soc=pronto
 
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0
+
+
 # BoringSSL Hacks
 PRODUCT_PACKAGES += \
     libboringssl-compat
