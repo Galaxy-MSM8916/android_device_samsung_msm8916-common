b7038bbe5c9cd78a017d6eca7d2b71ea14d9e7da
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-03 22:54:48 -0400

    msm8916: Update setup-makefiles and extract-files
     * Add support for specific section extraction
     * Update extract-files argument handling
     * Make spacing consistent
    
    Change-Id: I567764c29199256e175ec6a7794d755a38e908a8

--- extract-files.sh
+++ extract-files.sh
@@ -7,7 +7,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,29 +18,38 @@
 
 set -e
 
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CLEANUP=true
+function print_help() {
+    echo "Usage: `basename $0` [OPTIONS] "
+    echo "  -b | --setup-all  Set up all dirs (device and device-commons)"
+    echo "  -p | --path  Vendor blob source path (to ota package or system folder)"
+    echo "  -s | --section  Section to extract"
+    echo "  -c | --clean-vendor  Clean vendor dirs"
+    echo "  -h | --help Print this text"
+    exit 0
+}
 
 BOARD_COMMON=msm8916-common
 VENDOR=samsung
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 CM_ROOT="$MY_DIR"/../../..
 DEVICE_DIR="$MY_DIR"/../$DEVICE
 DEVICE_COMMON_DIR="$MY_DIR"/../$DEVICE_COMMON
 
+# determine which blob dirs to set up
 if [ -z "$SETUP_BOARD_COMMON_DIR" ]; then
-	SETUP_BOARD_COMMON_DIR=1
+    SETUP_BOARD_COMMON_DIR=1
 fi
 
 if [ -z "$SETUP_DEVICE_DIR" ]; then
-	SETUP_DEVICE_DIR=0
+    SETUP_DEVICE_DIR=0
 fi
 
 if [ -z "$SETUP_DEVICE_COMMON_DIR" ]; then
-	SETUP_DEVICE_COMMON_DIR=0
+    SETUP_DEVICE_COMMON_DIR=0
 fi
 
 HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
@@ -50,70 +59,50 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-while getopts ":nhsd:" options
-do
-  case $options in
-    b ) SETUP_ALL_DIRS=1 ;;
-    n ) CLEANUP="false" ;;
-    d ) SRC=$OPTARG ;;
-    s ) SETUP=1 ;;
-    h ) echo "Usage: `basename $0` [OPTIONS] "
-        echo "  -b  Set up all dirs (device and device-commons)"
-        echo "  -n  No cleanup"
-        echo "  -d  Fetch blob from filesystem"
-        echo "  -s  Setup only, no extraction"
-        echo "  -h  Show this help"
-        exit ;;
-    * ) ;;
-  esac
+# default to not sanitizing the vendor folder before extraction
+clean_vendor=false
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --setup-all )      SETUP_DEVICE_DIR=1
+                                SETUP_DEVICE_COMMON_DIR=1
+                                SETUP_BOARD_COMMON_DIR=1
+                                ;;
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                clean_vendor=false
+                                ;;
+        -c | --clean-vendor )   clean_vendor=true
+                                ;;
+        -h | --help )           print_help
+                                ;;
+    esac
+    shift
 done
 
-if [ -z $SRC ]; then
-  SRC=adb
+if [ -z "$SRC" ]; then
+    SRC=adb
 fi
 
-if [ -n "$SETUP_ALL_DIRS" ]; then
-	SETUP_DEVICE_DIR=1
-	SETUP_DEVICE_COMMON_DIR=1
-	SETUP_BOARD_COMMON_DIR=1
-fi
-
-if [ -n "$SETUP" ]; then
+if  [ "$SETUP_BOARD_COMMON_DIR" -eq 1 ]; then
     # Initialize the helper for common
-    if  [ "$SETUP_BOARD_COMMON_DIR" -eq 1 ]; then
-	setup_vendor "$BOARD_COMMON" "$VENDOR" "$CM_ROOT" true false
-    fi
-
-    if [ "$SETUP_DEVICE_COMMON_DIR" -eq 1 ] && [ -n "$DEVICE_COMMON" ] && [ -s $DEVICE_COMMON_DIR/proprietary-files.txt ]; then
-        # Initalize the helper for device
-        COMMON=1 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" false false
-    fi
-
-    if [ "$SETUP_DEVICE_DIR" -eq 1 ] && [ -n "$DEVICE" ] && [ -s $DEVICE_DIR/proprietary-files.txt ]; then
-        # Initalize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false false
-    fi
-
-    "$MY_DIR"/setup-makefiles.sh false
-
-else
-    # Initialize the helper for common
-    if  [ "$SETUP_BOARD_COMMON_DIR" -eq 1 ]; then
-	setup_vendor "$BOARD_COMMON" "$VENDOR" "$CM_ROOT" true "$CLEANUP"
-	extract "$MY_DIR"/proprietary-files.txt "$SRC"
-    fi
-
-    if [ "$SETUP_DEVICE_COMMON_DIR" -eq 1 ] && [ -n "$DEVICE_COMMON" ] && [ -s $DEVICE_COMMON_DIR/proprietary-files.txt ]; then
-        # Reinitialize the helper for device
-        COMMON=1 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" false "$CLEANUP"
-        extract $DEVICE_COMMON_DIR/proprietary-files.txt "$SRC"
-    fi
-
-    if [ "$SETUP_DEVICE_DIR" -eq 1 ] && [ -n "$DEVICE" ] && [ -s $DEVICE_DIR/proprietary-files.txt ]; then
-        # Reinitialize the helper for device
-        setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false "$CLEANUP"
-        extract $DEVICE_DIR/proprietary-files.txt "$SRC"
-    fi
-
-    "$MY_DIR"/setup-makefiles.sh "$CLEANUP"
+    setup_vendor "$BOARD_COMMON" "$VENDOR" "$CM_ROOT" true $clean_vendor
+    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 fi
+
+if [ "$SETUP_DEVICE_COMMON_DIR" -eq 1 ] && [ -s $DEVICE_COMMON_DIR/proprietary-files.txt ]; then
+    # Reinitialize the helper for device-common
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true $clean_vendor
+    extract $DEVICE_COMMON_DIR/proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+if [ "$SETUP_DEVICE_DIR" -eq 1 ] && [ -s $DEVICE_DIR/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
+    extract $DEVICE_DIR/proprietary-files.txt "$SRC" "$SECTION"
+fi
+
+"$MY_DIR"/setup-makefiles.sh
