08039edd0a68ce91d91791dc1c126ce2b39cf918
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-05 21:35:57 -0400

    msm8916: Remove sdk check script
     * This is no longer necessary since the OTA updater will
       give a warning if the user attempts to downgrade.
    
    Change-Id: I923f59b2b57a5435ad3bade6c1425d1cf644b077

--- product/releasetools.mk
+++ product/releasetools.mk
@@ -2,5 +2,4 @@
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/releasetools/run_scripts.sh:install/bin/run_scripts.sh \
 	$(LOCAL_PATH)/releasetools/functions.sh:install/bin/functions.sh \
-	$(LOCAL_PATH)/releasetools/installbegin/check_sdk_version.sh:install/bin/installbegin/check_sdk_version.sh \
 	$(LOCAL_PATH)/releasetools/postvalidate/resize_system.sh:install/bin/postvalidate/resize_system.sh
