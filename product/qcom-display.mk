6111f04aa45e067fedd45f3bcb10cd17b1bd68ce
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    msm8916: Update display/render for O
    * Add various HALs
    * Enable HWC2 & Gralloc1
    
    Change-Id: I907603a5cabc1756aefeb64ca1cd08d281c2cd79

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,8 +1,17 @@
 # Display
 PRODUCT_PACKAGES += \
-	copybit.msm8916 \
+	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service \
+	android.hardware.graphics.composer@2.1-impl \
+	android.hardware.graphics.mapper@2.0-impl \
+	android.hardware.memtrack@1.0-impl \
 	gralloc.msm8916 \
 	hwcomposer.msm8916 \
+	libgenlock \
 	libtinyxml \
 	libtinyxml2 \
 	memtrack.msm8916
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+	android.hardware.renderscript@1.0-impl
