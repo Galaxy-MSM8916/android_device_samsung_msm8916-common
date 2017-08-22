23f308937155f44b331fe92b8200f7b1ccfdfd4d
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    product: qcom-audio: Update audio for O
    * Build audio HALs
    
    Change-Id: I65598400ef5f56cf50a64337151d0a4d1db30977

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -22,3 +22,9 @@ PRODUCT_PACKAGES += \
 	tinyplay \
 	tinycap \
 	tinypcminfo
+
+PRODUCT_PACKAGES += \
+	android.hardware.audio@2.0-impl \
+	android.hardware.audio.effect@2.0-impl \
+	android.hardware.broadcastradio@1.0-impl \
+	android.hardware.soundtrigger@2.0-impl
