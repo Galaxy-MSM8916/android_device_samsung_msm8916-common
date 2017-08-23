8cf6555de69bc4d2605459cc396ffee3e7080f93
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-24 01:15:19 +0800

    msm8916-common: Build Sensors HAL
    Change-Id: Idedc3ba7737384f5b5476a36abf7db8e650ac067

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,3 +1,7 @@
+# Sensor HAL
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
