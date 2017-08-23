0d70ce92f0a8d1d63faeb4839ca96cdceffe0abf
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-24 01:56:50 +0800

    product: hidl: Set vendor headers
    Change-Id: I646ea0c3b3edcdfa8e6ae655a5391abdb1fcc96c

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,3 +1,6 @@
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
+# MSM8916 Headers
+PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8916/kernel-headers
