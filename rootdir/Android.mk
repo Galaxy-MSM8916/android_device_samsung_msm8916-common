a4a3942aed913e7252254b53c919f15abd5f4a0d
vlw <vlwwwwww@gmail.com>
2017-08-30 00:40:24 +0300

    msm8916-common: rootdir: Enable touch devices in recovery
     * The mdss driver calls mdss_dsi_panel_off() in the kernel
       when the display is initialised without calling mdss_dsi_panel_on()
       at boot even though the display will be active, so the function call
       to enable the touch devices is never made.
        
       This forcibly enables all touch devices in end of the boot process.
    
    Change-Id: I91ad4d4ecb187449a39ffaee441adc9313dbb86e
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -91,6 +91,16 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
+### init.recovery.qcom.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+######################
 ### init.qcom.sh
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sh
