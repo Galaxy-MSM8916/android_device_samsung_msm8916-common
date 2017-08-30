fad6b1b5caa08893c36bebce4c2c43e902102635
ZIM <zeustreker@gmail.com>
2017-08-30 12:02:06 +0000

    rootdir: Remove hcidump.sh
    Change-Id: I22172e159c8633edb210ad37df4223894531da1e

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -151,16 +151,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT)/etc/
 include $(BUILD_PREBUILT)
 
 ######################
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := hcidump.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/hcidump.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT)/etc/
-include $(BUILD_PREBUILT)
-
-######################
 ### init.qcom.bt.sh
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
