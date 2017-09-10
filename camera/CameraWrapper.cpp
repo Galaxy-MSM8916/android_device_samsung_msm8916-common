e9f2f3e4d9216b6bfc92b423da1ce5a3692e2609
vlw <vlwwwwww@gmail.com>
2017-09-10 18:01:47 +0300

    msm8916-common: Add custom CameraParameters lib
    Change-Id: I889678c57e1a7fa0f47b9a969cc737ddf1ee2ab7
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -32,7 +32,7 @@
 #include <hardware/camera.h>
 #include <camera/Camera.h>
 #include <camera/CameraParameters.h>
-#include <camera/CameraParametersExtra.h>
+#include <camera/CameraParameters_EXT.h>
 
 static android::Mutex gCameraWrapperLock;
 static camera_module_t *gVendorModule = 0;
