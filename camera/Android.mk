36f805a7be6fa1205d0e52e3f68108dfa87f8b38
vlw <vlwwwwww@gmail.com>
2017-09-10 18:01:47 +0300

    msm8916-common: Add custom CameraParameters lib
    Change-Id: I889678c57e1a7fa0f47b9a969cc737ddf1ee2ab7
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- camera/Android.mk
+++ camera/Android.mk
@@ -16,6 +16,19 @@ LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
 endif
 
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := \
+    frameworks/av/include
+
+LOCAL_SRC_FILES := \
+    CameraParameters.cpp \
+    CameraParameters_EXT.cpp
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libcamera_parameters_ext
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_STATIC_LIBRARY)
