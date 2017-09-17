eddf9046b673376440a686b56d474bb915756c4e
Ethan Chen <intervigil@gmail.com>
2017-09-17 00:27:22 -0700

    msm8916: Convert camera parameters library to soong
    Change-Id: I335950fd6eec03c4073c0500ce037117bb37ec3e

--- camera/Android.mk
+++ camera/Android.mk
@@ -20,18 +20,3 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 endif
-
-include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES := \
-    frameworks/av/include
-
-LOCAL_SRC_FILES := \
-    CameraParameters.cpp \
-    CameraParameters_EXT.cpp
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE := libcamera_parameters_ext
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_STATIC_LIBRARY)
