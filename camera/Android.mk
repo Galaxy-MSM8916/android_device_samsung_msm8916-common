775e572528a8c51493b7be4ce35479f023c8360e
Arne Coucheron <arco68@gmail.com>
2017-10-10 00:18:36 +0200

    msm8916-common: Move camera wrapper to vendor
    Change-Id: I6752fd7c9b918522e588fa830669a142f3fd7290

--- camera/Android.mk
+++ camera/Android.mk
@@ -3,29 +3,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-    framework/native/include \
-    system/media/camera/include
+    system/media/camera/include \
+    frameworks/native/include/media/openmax
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    liblog \
-    libcamera_client \
-    libgui \
-    libhidltransport \
-    libsensor \
-    libutils \
-    libcutils \
-    android.hidl.token@1.0-utils
+    libhardware liblog libcamera_client libutils libcutils libgui libsensor
 
-LOCAL_STATIC_LIBRARIES := \
-    libarect
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 endif
