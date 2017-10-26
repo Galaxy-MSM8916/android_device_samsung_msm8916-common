88d958d26ca3581889ca97614c966da0ca733971
SoUnd001 <sound0020@gmail.com>
2017-10-26 11:49:04 +0300

    msm8916: switch to LOS audio HAL
    Change-Id: I53af42b1508958e3b98675c8432a262e358a8dde

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,30 +1,23 @@
 # Audio
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	audio.primary.msm8916 \
-	audio.primary.default \
-	audio_policy.msm8916 \
-	audio.r_submix.default \
-	audio.tms.default \
-	audio.usb.default \
-	audiod \
-	libaudio-resampler \
-	libaudioroute \
-	libaudioutils \
-	libaudiopolicymanager \
-	libqcompostprocbundle \
-	libqcomvisualizer \
-	libqcomvoiceprocessing \
-	libqcmediaplayer \
-	libtinyalsa \
-	libtinycompress \
-	tinymix \
-	tinyplay \
-	tinycap \
-	tinypcminfo
+    audiod \
+    audio.a2dp.default \
+    audio.primary.msm8916 \
+    audio.primary.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudioresampler \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    tinyplay \
+    tinycap \
+    tinymix \
+    tinypcminfo \
+    libtinycompress
 
 PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl \
-	android.hardware.broadcastradio@1.0-impl \
-	android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
