6ae213fe2433d885c60b8063873ae6957fc78895
SoUnd001 <sound0020@gmail.com>
2017-10-31 14:34:11 +0200

    msm8916: Use a script for symlinking telephony db
    Change-Id: I9ebdaa0b79b867c0e399916432c9abec822cd7f5

---
+++ rootdir/etc/init.link_ril_db.sh
@@ -0,0 +1,63 @@
+#!/system/bin/sh
+
+LOG_TAG="link_telephony_dbs"
+LOG_NAME="${0}:"
+
+loge ()
+{
+  /system/bin/log -t $LOG_TAG -p e "$LOG_NAME $@"
+}
+
+logi ()
+{
+  /system/bin/log -t $LOG_TAG -p i "$LOG_NAME $@"
+}
+
+OLD_TELEPHONY_DB_DIR=/data/user_de/0/com.android.providers.telephony/databases/
+OLD_TELEPHONY_PREFS_DIR=/data/user_de/0/com.android.providers.telephony/shared_prefs/
+NEW_TELEPHONY_DB_DIR=/data/data/com.android.providers.telephony/databases/
+NEW_TELEPHONY_PREFS_DIR=/data/data/com.android.providers.telephony/shared_prefs/
+
+PATH_LEN=$(echo ${OLD_TELEPHONY_DB_DIR} | wc -c)
+PATH_LEN_PREFS=$(echo ${OLD_TELEPHONY_PREFS_DIR} | wc -c)
+
+RESTART_RIL=0
+
+logi "Starting Link RIL Databases"
+
+logi "Deleting directory..."
+rm -rf ${NEW_TELEPHONY_DB_DIR}
+rm -rf ${NEW_TELEPHONY_PREFS_DIR}
+
+logi "Creating directory..."
+mkdir -p ${NEW_TELEPHONY_DB_DIR}
+mkdir -p ${NEW_TELEPHONY_PREFS_DIR}
+
+for db in `find ${OLD_TELEPHONY_DB_DIR} -type f | cut -c ${PATH_LEN}-`; do
+    if ! [ -e ${NEW_TELEPHONY_DB_DIR}/${db} ]; then
+	    logi "Linking ${NEW_TELEPHONY_DB_DIR}${db}..."
+	    ln -s ${OLD_TELEPHONY_DB_DIR}${db} ${NEW_TELEPHONY_DB_DIR}
+    fi
+done
+
+for prefs in `find ${OLD_TELEPHONY_PREFS_DIR} -type f | cut -c ${PATH_LEN_PREFS}-`; do
+    if ! [ -e ${NEW_TELEPHONY_PREFS_DIR}/${prefs} ]; then
+	    logi "Linking ${NEW_TELEPHONY_PREFS_DIR}${prefs}..."
+	    ln -s ${OLD_TELEPHONY_PREFS_DIR}${prefs} ${NEW_TELEPHONY_PREFS_DIR}
+	    RESTART_RIL=1
+    fi
+done
+
+logi "Setting permissions..."
+chmod 0751 ${NEW_TELEPHONY_DB_DIR}/..
+chmod 0771 ${NEW_TELEPHONY_DB_DIR}
+chmod 0751 ${NEW_TELEPHONY_PREFS_DIR}/..
+chmod 0771 ${NEW_TELEPHONY_PREFS_DIR}
+chown radio:radio ${NEW_TELEPHONY_DB_DIR} -R
+chown radio:radio ${NEW_TELEPHONY_PREFS_DIR} -R
+
+if [ "$RESTART_RIL" -eq 1 ]; then
+    logi "Restarting RIL..."
+    restart ril-daemon
+    restart ril-daemon1
+fi
