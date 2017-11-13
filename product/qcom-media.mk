c255c3448c25dda30910b0208fc9295e6e98ea18
Dyneteve <sound0020@gmail.com>
2017-11-13 12:58:29 +0000

    msm8916: Remove unused media codecs
    Change-Id: Idd8fe281031ca824494862f43979006491cb5e99

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -10,8 +10,6 @@ PRODUCT_COPY_FILES += \
 endif
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/media/media_codecs_sec_primary.xml:system/etc/media_codecs_sec_primary.xml \
-	$(LOCAL_PATH)/configs/media/media_codecs_sec_secondary.xml:system/etc/media_codecs_sec_secondary.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
