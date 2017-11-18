360c1966640758f803b99c3fbe2600dc3eea2b20
danile71 <danile71@gmail.com>
2017-11-18 11:34:32 +0300

    board: Use OSS audio
     * Remove realcall ril class code
     * Disable ril class by default
     * Add OSS audio hal board flags
     
    Change-Id: I0b07e3f16ed0f2d11a50372a357b42e0e173e061

--- ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungQcomRIL.java
@@ -20,7 +20,6 @@ package com.android.internal.telephony;
 import static com.android.internal.telephony.RILConstants.*;
 
 import android.content.Context;
-import android.media.AudioManager;
 import android.telephony.Rlog;
 import android.os.AsyncResult;
 import android.os.Message;
@@ -50,8 +49,6 @@ import android.os.RemoteException;
  */
 public class SamsungQcomRIL extends RIL {
 
-    AudioManager mAudioManager;
-
     public SamsungQcomRIL(Context context, int networkMode, int cdmaSubscription) {
         this(context, networkMode, cdmaSubscription, null);
     }
@@ -59,78 +56,6 @@ public class SamsungQcomRIL extends RIL {
     public SamsungQcomRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
-        
-
-        Rlog.e(RILJ_LOG_TAG, "Setting mAudioManager..");
-        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-    }
-
-    /* Toggles the loud speaker state.
-     * This is a hack to get in-call sound working.
-     */
-    private void toggleSpeaker() {
-        if (mAudioManager.isSpeakerphoneOn()) {
-            Rlog.e(RILJ_LOG_TAG, "Speaker is on. Setting to: off");
-            mAudioManager.setSpeakerphoneOn(false);
-        }
-        else {
-            Rlog.e(RILJ_LOG_TAG, "Speaker is off. Setting to: on");
-            mAudioManager.setSpeakerphoneOn(true);
-        }
-    }
-
-    /*
-     * Samsung-specific modification to enable call audio routing.
-     */
-    private void setRealCall(boolean value) {
-        if (value) {
-            Rlog.e(RILJ_LOG_TAG, "Setting realcall param to: on");
-            mAudioManager.setParameters("realcall=on");
-	    /* toggle speaker twice */
-            toggleSpeaker();
-            toggleSpeaker();
-        }
-        else {
-            Rlog.e(RILJ_LOG_TAG, "Setting realcall param to: off");
-            mAudioManager.setParameters("realcall=off");
-        }
-    }
-
-    @Override
-    public void
-    dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
-        super.dial(address, clirMode, uusInfo, result);
-        setRealCall(true);
-    }
-
-    @Override
-    public void
-    hangupConnection (int gsmIndex, Message result) {
-           super.hangupConnection(gsmIndex, result);
-           setRealCall(false);
-    }
-
-
-    @Override
-    public void
-    hangupForegroundResumeBackground (Message result) {
-            super.hangupForegroundResumeBackground(result);
-            setRealCall(true);
-    }
-
-
-    @Override
-    public void
-    switchWaitingOrHoldingAndActive (Message result) {
-            super.switchWaitingOrHoldingAndActive(result);
-            setRealCall(true);
-    }
-
-    @Override
-    public void
-    rejectCall (Message result) {
-            super.rejectCall(result);
-            setRealCall(false);
     }
 
     @Override
