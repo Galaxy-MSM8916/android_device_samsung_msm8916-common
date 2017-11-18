6b320a0d39e5f26ea0ad2e1b5dbf743f446573db
danile71 <danile71@gmail.com>
2017-11-18 11:34:32 +0300

    board: Use OSS audio
     * Remove realcall ril class code
     * Add OSS audio hal board flags
     * Add audio platform info config
    
    Change-Id: I0b07e3f16ed0f2d11a50372a357b42e0e173e061

--- board/qcom-audio.mk
+++ board/qcom-audio.mk
@@ -3,7 +3,7 @@ USE_CUSTOM_AUDIO_POLICY := 1
 BOARD_USES_GENERIC_AUDIO := true
 TARGET_USES_QCOM_MM_AUDIO := true
 USE_XML_AUDIO_POLICY_CONF := 1
-
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
 
 
 CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
@@ -12,6 +12,10 @@ CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
 PRODUCT_COPY_FILES += \
     $(CONFIG_PATH)/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf \
     $(CONFIG_PATH)/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+# Platform info config
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
 #XML Audio configuration files
 ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
