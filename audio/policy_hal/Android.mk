c70257766ffc2c8b0d7f44450a10be53e61dcbe4
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-01-14 20:27:37 -0500

    msm8916: Use custom audio hals
    Change-Id: I98c0516942e96bd3b93c7457bb92b707eebd3a69

--- audio/policy_hal/Android.mk
+++ audio/policy_hal/Android.mk
@@ -11,8 +11,8 @@
 # production of derivative works therefrom without the express permission of
 # the copyright owners is prohibited.
 #
-ifneq ($(USE_LEGACY_AUDIO_POLICY), 1)
-ifeq ($(USE_CUSTOM_AUDIO_POLICY), 1)
+ifeq ($(BOARD_USES_CUSTOM_AUDIO), true)
+ifeq ($(USE_CUSTOM_AUDIO_POLICY_HAL), 1)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
