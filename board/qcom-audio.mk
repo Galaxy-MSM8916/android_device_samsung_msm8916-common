bc2c951af7f2ce5db6804b8a1e838debf39ec4ba
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-01-14 20:49:46 -0500

    msm8916: Add audio configs and clean up makefiles
     * Add qcom audio configs and clean up audio makefiles
    
    Change-Id: Ibf0dfc13576e1a975f9777cd793174f49c552fb0

--- board/qcom-audio.mk
+++ board/qcom-audio.mk
@@ -6,26 +6,3 @@ BOARD_USES_GENERIC_AUDIO := false
 TARGET_USES_QCOM_MM_AUDIO := true
 USE_XML_AUDIO_POLICY_CONF := 1
 AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
-
-
-CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
-
-# Audio configuration file
-PRODUCT_COPY_FILES += \
-    $(CONFIG_PATH)/msm8916_32/audio_policy.conf:system/etc/audio_policy.conf \
-    $(CONFIG_PATH)/msm8916_32/audio_effects.conf:system/vendor/etc/audio_effects.conf
-
-# Mixer paths
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
-#XML Audio configuration files
-ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
-PRODUCT_COPY_FILES += \
-    $(TOPDIR)$(CONFIG_PATH)/msm8916_32/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
-endif
