fc8da933ce950c75c5d078979d5b0d6a2ac50ec5
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-01-15 02:11:07 -0500

    audio: hal: Fix property_get_int32 function call signature
    Change-Id: I98cff608a43c2a8d62679ba3ae7f29c5cbf2f910

--- audio/hal/voice_extn/msim_voice_extn.c
+++ audio/hal/voice_extn/msim_voice_extn.c
@@ -85,7 +85,7 @@ int msim_voice_extn_set_parameters(struct audio_device *adev __unused,
                             sizeof(value));
     if (ret >= 0) {
 #ifdef SAMSUNG_DUAL_SIM
-        msim_phone_type = property_get_int32(
+        msim_phone_type = property_get_int32(AUDIO_PARAMETER_VALUE_CP2,
                 strcmp(value, AUDIO_PARAMETER_VALUE_CP2) ?
                 AUDIO_PROPERTY_SEC_VSID1 : AUDIO_PROPERTY_SEC_VSID2) + 1;
 #endif
