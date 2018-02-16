9fde1197f3039a81084ea8e6a5204de10773a01a
Christopher N. Hesse <raymanfx@gmail.com>
2018-02-16 02:06:05 +0100

    msm8916-common: Add libstagefright shim for NV21 color format
    Original by rayman (https://github.com/LineageOS/android_device_samsung_universal7880-common/commit/1e33b72d6adf22333abe4cc8c6a6d27969026786)
    
    Edited for zero-common to support non-LineageOS-ROMs
    
    Change-Id: Ic0cd891819acd27d63ac4cdb3078aff395d544f5

--- product/camera.mk
+++ product/camera.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
+# Stagefright-shims
+PRODUCT_PACKAGES += \
+	libstagefright_shim
+
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl-legacy \
