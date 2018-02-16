621155503881d0ddb907af68fa611640df1e01bd
Christopher N. Hesse <raymanfx@gmail.com>
2018-02-16 02:06:05 +0100

    msm8916-common: Add libstagefright shim for NV21 color format
    Original by rayman (https://github.com/LineageOS/android_device_samsung_universal7880-common/commit/1e33b72d6adf22333abe4cc8c6a6d27969026786)
    
    Edited for zero-common to support non-LineageOS-ROMs
    
    Change-Id: Ic0cd891819acd27d63ac4cdb3078aff395d544f5

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,26 +1,41 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
-
 LOCAL_SRC_FILES := \
     gps_shim.cpp
 
 LOCAL_MODULE := libshim_gps
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils libbinder libutils
-
 LOCAL_SRC_FILES := \
     secril_shim.cpp
 
 LOCAL_MODULE := libshim_secril
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := CameraSource.cpp
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/frameworks/av/include \
+    $(TOP)/frameworks/native/include/media/hardware \
+    $(TOP)/frameworks/native/include/media/openmax \
+    $(TOP)/frameworks/native/libs/arect/include \
+    $(TOP)/frameworks/native/libs/nativebase/include
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.graphics.bufferqueue@1.0 \
+    android.hidl.token@1.0-utils \
+    libbase \
+    libcamera_client \
+    liblog
+
+LOCAL_MODULE := libstagefright_shim
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
