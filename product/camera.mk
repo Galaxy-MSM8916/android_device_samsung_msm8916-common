40ab41d304c9d46e7b36cb93fb4eabd784e89cd5
Lukas0610 <mail@lukasberger.at>
2018-03-03 18:58:28 +0100

    msm8916-common: LD-PRELOAD stagefright-shim into mediaserver
    Change-Id: If8fa6044a36a2b0fe234b415d17a5d37f7beed7c

--- product/camera.mk
+++ product/camera.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/mediaserver.rc:system/etc/init/mediaserver.rc
+
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl-legacy \
