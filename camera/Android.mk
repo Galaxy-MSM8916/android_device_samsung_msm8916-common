4d0e77aeba90cb737b670d1e5e7c63e30d7ec85c
Dyneteve <sound0020@gmail.com>
2018-03-06 13:53:51 +0100

    [DNM] Oreo fixes
    Change-Id: Ib7da98e2a0577ade94a02fbe7790bad1273a135d

--- camera/Android.mk
+++ camera/Android.mk
@@ -8,14 +8,19 @@ LOCAL_C_INCLUDES := \
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
-LOCAL_HEADER_LIBRARIES += \
-    libnativebase_headers
-
-LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libdl \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    libcutils \
+    android.hidl.token@1.0-utils
+
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
