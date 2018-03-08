ef8e558cd3862dd58a35960c28978cb4ab912b6b
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2018-03-08 11:45:20 +0200

    msm8916: Move configs to /vendor

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -1,7 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
 # Wifi configuration files
 PRODUCT_COPY_FILES += \
