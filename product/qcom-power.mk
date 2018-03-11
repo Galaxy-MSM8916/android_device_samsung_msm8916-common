72700ae73e85ba80e01fe5e62386b51a2627859e
Paul Keith <javelinanddart@gmail.com>
2018-03-11 05:12:09 +0100

    msm8916-common: Use the new binderized QTI HIDL Power HAL
    Change-Id: I7301b3304f171e2adb0467ba7edb7b2f2a860a14

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -4,10 +4,4 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-	android.hardware.power@1.0-impl \
-	android.hardware.power@1.0-service
-
-ifneq ($(filter j7ltespr j7ltechn,$(TARGET_DEVICE)),)
-PRODUCT_PACKAGES += \
-	power.msm8916
-endif
+	android.hardware.power@1.0-service-qti
