43f709b62e9a806c9e39d8ae53c3f5d74d330fa4
Dyneteve <sound0020@gmail.com>
2018-03-14 10:06:39 -0400

    msm8916: Preload boringssl
    Change-Id: Ib0c8aa36446ccd4be2ce8dbe6fc38bd187ef9426

--- board/shim.mk
+++ board/shim.mk
@@ -1,3 +1,6 @@
 # Shims
 TARGET_LD_SHIM_LIBS := /system/lib/libsec-ril.so|libshim_secril.so:/system/lib/libsec-ril-dsds.so|libshim_secril.so:/system/vendor/lib/libizat_core.so|libshim_gps.so
 
+# Sp-shim
+TARGET_LD_PRELOAD := /system/lib/libboringssl-compat.so
+
