a82696d1720683c7bd27eeb6e40ae1f4248b7fc3
Dyneteve <sound0020@gmail.com>
2018-03-16 07:18:09 -0400

    msm8916-common: Do not use legacy cam impl
    Change-Id: I3aa9f675deb8a466da6d502fbbba07c685e69132

--- product/camera.mk
+++ product/camera.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-	android.hardware.camera.provider@2.4-impl-legacy \
-        camera.device@1.0-impl \
+	android.hardware.camera.provider@2.4-impl \
+	camera.device@1.0-impl \
 	libmm-qcamera \
 	camera.msm8916
