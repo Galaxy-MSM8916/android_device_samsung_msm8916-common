cf5f450a968035f354113f9379bb77b8deaac38a
Dyneteve <sound0020@gmail.com>
2018-03-19 09:02:51 -0400

    [DNM] Build twrp
    Change-Id: I24f216a03e1e7746c3aaa661a2ab74589c5b217a

--- board/01-recovery.mk
+++ board/01-recovery.mk
@@ -1,8 +1,5 @@
 # TWRP
-#ifneq ($(wildcard bootable/recovery-twrp),)
-#	RECOVERY_VARIANT := twrp
-#endif
-
+RECOVERY_VARIANT := twrp
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT 	    := \"roboto_23x41.h\"
