e93d6954d71d050b1f853a051f7e9cd4e9e3ecfb
Dyneteve <sound0020@gmail.com>
2018-03-29 13:59:55 -0400

    msm8916: Add j5x* to barrier
    Change-Id: If15e76bc942687184edf84ef9a49efd09bed5f98

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,7 +19,7 @@
 set -e
 
 BOARD_COMMON=msm8916-common
-DEVICES_ALL="gprimelte gprimeltespr gprimeltexx gtelwifiue gtesqltespr j53gxx j5lte j5ltechn j5nlte j7ltespr j7ltechn o7prolte"
+DEVICES_ALL="gprimelte gprimeltespr gprimeltexx gtelwifiue gtesqltespr j53gxx j5lte j5ltechn j5nlte j7ltespr j7ltechn o7prolte j5xnlte j5xlte"
 VENDOR=samsung
 
 INITIAL_COPYRIGHT_YEAR=2017
