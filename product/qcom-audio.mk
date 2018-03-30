1891902ebad65772e7a45aff3026f6df627665c8
Alberto97 <albertop2197@gmail.com>
2018-03-30 15:37:36 +0200

    msm8916-comon: Add binderized audio service
    Change-Id: I211f854709f86d72769c083087591bbc0e2569d8

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,8 +1,6 @@
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    audio.primary.msm8916 \
-    audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudioresampler \
@@ -19,4 +17,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
