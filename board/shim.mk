a44d3b73d95dd009d90606df4746fb2733045485
Dyneteve <sound0020@gmail.com>
2018-03-30 10:30:42 -0400

    msm8916: Add shim for camera
    Change-Id: I2c2f09e6fd7b9230973ad99528618a30906f8c3f

--- board/shim.mk
+++ board/shim.mk
@@ -1,2 +1,2 @@
 # Shims
-TARGET_LD_SHIM_LIBS := /system/lib/libcrypto.so|libboringssl-compat.so:/system/lib/libmmjpeg_interface.so|libboringssl-compat.so:/system/lib/libsec-ril.so|libshim_secril.so:/system/lib/libsec-ril-dsds.so|libshim_secril.so:/system/vendor/lib/libizat_core.so|libshim_gps.so
+TARGET_LD_SHIM_LIBS := /system/lib/libcrypto.so|libboringssl-compat.so:/system/lib/libmmjpeg_interface.so|libboringssl-compat.so:/system/lib/libsec-ril.so|libshim_secril.so:/system/lib/libsec-ril-dsds.so|libshim_secril.so:/system/vendor/lib/libizat_core.so|libshim_gps.so:/system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so
