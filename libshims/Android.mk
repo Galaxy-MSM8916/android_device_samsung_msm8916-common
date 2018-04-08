4e4456d2343c711a94d0bda4402277f8be6f3fc5
Dyneteve <sound0020@gmail.com>
2018-04-08 12:37:37 -0400

    msm8916: Move all the shims to vendor
    Change-Id: Ib84ef92aefdd6956c2b15a5a39347caa4fba7c6c

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,8 +6,7 @@ LOCAL_SRC_FILES := \
     gps_shim.cpp
 
 LOCAL_MODULE := libshim_gps
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -16,7 +15,7 @@ LOCAL_SRC_FILES := \
     secril_shim.cpp
 
 LOCAL_MODULE := libshim_secril
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -28,23 +27,20 @@ LOCAL_C_INCLUDES := \
     $(TOP)/frameworks/native/include/media/openmax \
     $(TOP)/frameworks/native/libs/arect/include \
     $(TOP)/frameworks/native/libs/nativebase/include
-
+    
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.graphics.bufferqueue@1.0 \
     android.hidl.token@1.0-utils \
     libbase \
     libcamera_client \
     liblog
-
 LOCAL_MODULE := libstagefright_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := camera_shim.c
 LOCAL_MODULE := libcamera_shim
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
