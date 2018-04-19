0477003ca2a0346b9d302d10ff4b08bfbb056a45
AdrianDC <radian.dc@gmail.com>
2018-04-19 12:55:50 +0000

    CameraWrapper: update library dependencies for O
    Change-Id: Ia4f3cfac349f8a1d4592c28acabd0e6d8ea3eb68

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,15 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/media/camera/include
+LOCAL_C_INCLUDES := \
+    framework/native/include \
+    system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libgui libsensor
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    android.hidl.token@1.0-utils
 
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_STATIC_LIBRARIES := \
+    libarect
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
