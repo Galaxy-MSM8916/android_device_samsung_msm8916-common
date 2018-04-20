8ef22bd7558b665b11744345bb6fc21fa7f2c86f
tangalbert919 <tangalbert919@gmail.com>
2018-04-19 20:18:52 -0500

    [DNM] Do 64-bit builds
    Absolutely do not merge because this is purely experimental, it
    probably won't work, and unless proven wrong with a benchmark,
    there's no real benefit to this (other than finally being able to
    use 64-bit applications).
    
    Change-Id: Ice52827ab6291053ce9ef99ad873cf04c1a527fd

--- board/kernel.mk
+++ board/kernel.mk
@@ -10,6 +10,8 @@ BOARD_KERNEL_CMDLINE += \
 
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_CUSTOM_BOOTIMG := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -21,8 +23,4 @@ TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
 BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
-
-ifneq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin),)
-    KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin
-    KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-endif
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
