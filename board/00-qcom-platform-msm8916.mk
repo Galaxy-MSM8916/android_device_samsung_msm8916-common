36e62f40c0c9ac960123efb8e18becadc2b29727
tangalbert919 <tangalbert919@gmail.com>
2018-04-19 20:18:52 -0500

    [DNM] Do 64-bit builds
    Absolutely do not merge because this is purely experimental, it
    probably won't work, and unless proven wrong with a benchmark,
    there's no real benefit to this (other than finally being able to
    use 64-bit applications).
    
    Change-Id: Ice52827ab6291053ce9ef99ad873cf04c1a527fd

--- board/00-qcom-platform-msm8916.mk
+++ board/00-qcom-platform-msm8916.mk
@@ -1,13 +1,27 @@
-FORCE_32_BIT := true
+FORCE_32_BIT := false
 
 # Inherit from common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
 # Platform
+
+ifeq ($(FORCE_32_BIT),true)
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+
+else
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_ARCH_VARIANT := armv8-a
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+endif
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU       := qcom-adreno306
 BOARD_VENDOR := samsung
