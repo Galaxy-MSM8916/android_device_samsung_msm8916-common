120e5797d03848c2cc6670a4cec04901ce11db73
Dyneteve <sound0020@gmail.com>
2018-04-24 11:52:04 -0400

    msm8916: Properly fix camera for NV21 and NV21E format
    Change-Id: I888270d61c939e376589266160793bf96cd95890

--- product/camera.mk
+++ product/camera.mk
@@ -2,9 +2,6 @@
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/mediaserver.rc:system/etc/init/mediaserver.rc
-
 # Stagefright-shims
 PRODUCT_PACKAGES += \
 	libcamera_shim \
