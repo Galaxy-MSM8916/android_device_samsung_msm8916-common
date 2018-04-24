91535bea56a33a90df31593e75be203af857774c
Dyneteve <sound0020@gmail.com>
2018-04-24 11:52:04 -0400

    msm8916: Properly fix camera for NV21 and NV21E format
    Change-Id: I888270d61c939e376589266160793bf96cd95890

--- board/shim.mk
+++ board/shim.mk
@@ -4,5 +4,5 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libmmjpeg_interface.so|libboringssl-compat.so \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
-    /system/vendor/lib/libizat_core.so|libshim_gps.so \
-    /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so
+    /system/lib/libstagefright.so|libstagefright_shim.so \
+    /system/vendor/lib/libizat_core.so|libshim_gps.so
