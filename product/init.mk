5b21821600d78c74077e50b62bc83fc45e6b27d5
Dyneteve <sound0020@gmail.com>
2018-05-19 13:45:22 -0400

    msm8916: Import powerhal
    Change-Id: I8d98af66144a2b439789e7993d302982bb3d3819

--- product/init.mk
+++ product/init.mk
@@ -4,7 +4,11 @@ PRODUCT_PACKAGES += \
 	init.carrier.rc \
 	init.qcom.ril.sh \
 	init.qcom.post_boot.sh \
+ifeq ($(filter j7ltespr j7ltechn,$(TARGET_DEVICE)),)
+	init.qcom.power.rc \
+else
 	init.qcom.early_boot.sh \
+endif
 	init.link_ril_db.sh \
 	init.qcom.usb.rc \
 	init.qcom.usb.sh \
