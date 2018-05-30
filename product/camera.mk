652859e051535b5df74b5124545802ea855465d5
Dyneteve <sound0020@gmail.com>
2018-05-30 12:53:29 -0400

    msm8916: Import OSS camera hal
    Change-Id: If9d282497c6c672b063dc9725132e0bce4138ec5

--- product/camera.mk
+++ product/camera.mk
@@ -1,14 +1,10 @@
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
-
-# Stagefright-shims
-PRODUCT_PACKAGES += \
-	libcamera_shim
-
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
-	camera.device@1.0-impl \
+	android.hardware.camera.provider@2.4-service \
+	camera.device@3.2-impl \
 	libmm-qcamera \
 	camera.msm8916
