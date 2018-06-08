8e56a912f9608d7fe4f61edd0d52243639b87803
Dyneteve <sound0020@gmail.com>
2018-06-08 05:44:13 -0400

    msm8916: Update blobs
    Change-Id: I83e3adea94c76b3d2b2fa224b5e78cff7babdf1f

---
+++ board/hidl.mk
@@ -0,0 +1,2 @@
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(LOCAL_PATH)/compatibility_matrix.xml
--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -17,9 +17,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
