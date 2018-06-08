e0cc9c1afcc546409a9868884553bca5dd6d09b9
Dyneteve <sound0020@gmail.com>
2018-06-08 05:44:13 -0400

    msm8916: Update blobs
    Change-Id: I83e3adea94c76b3d2b2fa224b5e78cff7babdf1f

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,6 +1,2 @@
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 # MSM8916 Headers
 PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8916/kernel-headers
