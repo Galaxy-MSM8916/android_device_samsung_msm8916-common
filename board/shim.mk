90201a4be7e4b3e90778795ce4fa4a2d77bcb557
Dyneteve <sound0020@gmail.com>
2018-06-22 06:06:47 -0400

    msm8916: Get rid of libboringssl-compat
    Change-Id: Ia56ea6b417531746f5236a757f4708dcb6de1ea2

--- board/shim.mk
+++ board/shim.mk
@@ -1,7 +1,5 @@
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libcrypto.so|libboringssl-compat.so \
-    /system/lib/libmmjpeg_interface.so|libboringssl-compat.so \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
