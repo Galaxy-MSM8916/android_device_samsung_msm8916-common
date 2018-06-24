f3e56fb3d68d765de97ff9ad153094e35c5db35e
Vincent <vincent.zvikaramba@mail.utoronto.ca>
2018-06-24 19:18:28 -0400

    Revert "msm8916: Use qcom mixer paths only for the devices that want it"
    This reverts commit d180a42ca28d2a03f51317f07c2f56f7a5829c5b.
    
    Change-Id: I9585cc1a756887dd583d8eaf7b916f76c547ae70

--- board/qcom-audio.mk
+++ board/qcom-audio.mk
@@ -11,3 +11,9 @@ CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(CONFIG_PATH)/msm8916_32/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
+
+# Mixer paths
+ifeq ($(USE_QCOM_MIXER_PATHS), false)
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+endif
