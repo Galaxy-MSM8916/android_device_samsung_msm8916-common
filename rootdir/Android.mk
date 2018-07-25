b500e36db47d031313291b4a7a04cea0c5c2f9f6
Vincent <vincent.zvikaramba@mail.utoronto.ca>
2018-07-25 13:30:36 -0400

    Revert "msm8916: Cleanup ramdisk"
     * Restore init.qcom.sh
    
    This (partially) reverts commit 73405abca482058a42a935dda1c1916809ce80dd.
    
    Change-Id: I5374b84e3a8fdf63879d98a3fbd8589464f8e54f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -90,6 +90,16 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
+### init.qcom.sh
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+######################
 ### ueventd.qcom.rc
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
