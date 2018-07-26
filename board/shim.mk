488e8fec46390f8c13ae1e557160f46e52d844c6
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-26 07:26:22 -0400

    msm8916: Shim libqomx_jpegenc
    Change-Id: I65402616e0699110cf62b6907e67123417c0cad6

--- board/shim.mk
+++ board/shim.mk
@@ -4,4 +4,5 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
-    /system/vendor/lib/libizat_core.so|libshim_gps.so
+    /system/vendor/lib/libizat_core.so|libshim_gps.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
