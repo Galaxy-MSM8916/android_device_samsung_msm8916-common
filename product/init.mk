22841c59609924da8bc289a33dadfcfba8de2c0f
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-26 07:37:15 -0400

    msm8916: Remove init.qcom.power.rc
     * Duplicates functions of init.qcom.post_boot.sh
    
    Change-Id: I9db7a29581a5037661cfa6090b6f409077d288b5

--- product/init.mk
+++ product/init.mk
@@ -4,6 +4,7 @@ PRODUCT_PACKAGES += \
 	init.carrier.rc \
 	init.qcom.ril.sh \
 	init.qcom.early_boot.sh \
+	init.qcom.post_boot.sh \
 	init.link_ril_db.sh \
 	init.qcom.usb.rc \
 	init.qcom.usb.sh \
@@ -12,11 +13,3 @@ PRODUCT_PACKAGES += \
 	init.recovery.qcom.rc \
 	twrp.fstab \
 	ueventd.qcom.rc
-
-ifeq ($(filter j7ltespr j7ltechn,$(TARGET_DEVICE)),)
-PRODUCT_PACKAGES += \
-	init.qcom.power.rc
-else
-PRODUCT_PACKAGES += \
-	init.qcom.post_boot.sh
-endif
