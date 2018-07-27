daff22806651f3a491d5f129569709560f081376
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-07-27 12:38:23 -0400

    Revert "msm8916: Set ro.alarm_boot in init"
    This reverts commit ddd6d530db3e0ce5d6f6a0b56a1f9108bb961057.
    
    Change-Id: Ia3ecbbbecd9f95b99b1d66524aea3557271d79b4

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_C_INCLUDES := \
-	system/core/base/include \
 	system/core/init \
 	external/selinux/libselinux/include
 
@@ -15,6 +14,6 @@ ifneq ($(TARGET_LIBINIT_MSM8916_DEFINES_FILE),)
   LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8916_DEFINES_FILE)
 endif
 LOCAL_MODULE := libinit_msm8916
-LOCAL_STATIC_LIBRARIES := libbase libselinux
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
