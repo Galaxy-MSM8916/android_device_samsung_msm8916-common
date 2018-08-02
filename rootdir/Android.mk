802e8d66b89804fd4b87f664a90f6b17290ebdee
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-02 10:07:48 -0400

    Revert "msm8916: Correctly start ril-daemon2"
    This reverts commit a0dc295e4e405bf300db91b3014de1dbba828b0d.
    
    Change-Id: If77d638fc81f36e16e0ea7f0d749d9a0b82b04d5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,9 +11,9 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 ######################
-### init.qcom.ril.sh
+### init.class_main.sh
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.ril.sh
+LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
