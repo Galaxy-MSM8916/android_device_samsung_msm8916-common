88ace69a00df02e92cb47f62f102499f19153c33
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-11 22:09:06 -0400

    msm8916: rootdir: link_ril: Use local module name var in LOCAL_SRC_FILES
    Change-Id: I849d93438d58571b5a733dae77bff42655153649

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -16,7 +16,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.link_ril_db.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.link_ril_db.sh
+LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
