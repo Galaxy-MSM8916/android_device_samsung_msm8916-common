ce8fddf77744e4f07515dd6513bb7269ceb0b26e
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-13 01:48:19 -0400

    msm8916: board: Remove libcrypto's boringssl shim
      - Shim libqomx_jpegenc instead, the library which
        depends on the functions in libboringssl
    
      - This should fix the encoding failure that causes the camera
        to fail to save images/videos.
    
    Change-Id: I48a755dd8fbb309d35f52f0c5221d485c6453500

--- board/shim.mk
+++ board/shim.mk
@@ -1,8 +1,8 @@
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libcrypto.so|libboringssl-compat.so \
     /system/lib/libmmjpeg_interface.so|libboringssl-compat.so \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
-    /system/vendor/lib/libizat_core.so|libshim_gps.so
+    /system/vendor/lib/libizat_core.so|libshim_gps.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
