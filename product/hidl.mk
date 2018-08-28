366037ff6bdadc8805a27d91e1aa4410158d5887
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-08-27 23:27:55 -0400

    msm8916-common: Use DEVICE_MANIFEST_FILE to specify manifest
     - Instead of copying it over in msm8916.mk
    
    Change-Id: I2bc00260854c57b291e7c8750115aafb70546f21

--- product/hidl.mk
+++ product/hidl.mk
@@ -1,6 +1,5 @@
 # HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
 
 # MSM8916 Headers
 PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8916/kernel-headers
