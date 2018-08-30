8a9d92fa1736f21334c2a720ce4a03bc89e0a9fd
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-08-30 00:25:35 -0400

    msm8916-common: camera: Switch to normal (non-legacy) camera impl
    Change-Id: I9ed2f9ed62a3142a9b7c44c49539a893a6c7a552

--- product/camera.mk
+++ product/camera.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-	android.hardware.camera.provider@2.4-impl-legacy \
+	android.hardware.camera.provider@2.4-impl \
         camera.device@1.0-impl \
 	libmm-qcamera \
 	camera.msm8916
