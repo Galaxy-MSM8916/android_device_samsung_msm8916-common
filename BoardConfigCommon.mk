fc606b83353672e673931ec16b8afc63f095bad9
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-20 18:01:56 -0400

    msm8916-common: Remove board/product fragments
     - Merge with BoardConfigCommon.mk and msm8916.mk respectively
    
    Change-Id: I1dab1a34c4afc9b945673c836f9deba6e0667459

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,13 +13,286 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PLATFORM_PATH := device/samsung/msm8916-common
+LOCAL_PATH := device/samsung/msm8916-common
 
 # Includes
-TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Inherit from common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
 
-# Include board config fragments
-include $(PLATFORM_PATH)/board/*.mk
+# Architecture/platform
+BOARD_VENDOR := samsung
+FORCE_32_BIT := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU       := qcom-adreno306
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+
+# ART
+WITH_DEXPREOPT := false
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+
+# Audio
+AUDIO_CONFIG_PATH := hardware/qcom/audio-caf/msm8916/configs
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_GENERIC_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
+USE_CUSTOM_AUDIO_POLICY := 1
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# Mixer paths
+ifneq ($(USE_CUSTOM_MIXER_PATHS), true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+endif
+
+#XML Audio configuration files
+ifeq ($(USE_XML_AUDIO_POLICY_CONF), 1)
+PRODUCT_COPY_FILES += \
+    $(TOPDIR)$(AUDIO_CONFIG_PATH)/msm8916_32/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(TOPDIR)$(AUDIO_CONFIG_PATH)/msm8916_32/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+endif
+
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+QCOM_BT_USE_BTNV := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
+
+# Bootloader
+BOARD_PROVIDES_BOOTLOADER_MESSAGE := false
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+
+# Camera
+BOARD_GLOBAL_CFLAGS += -DMETADATA_CAMERA_SOURCE
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE := true
+TARGET_PROVIDES_CAMERA_HAL := true
+TARGET_USE_VENDOR_CAMERA_EXT := true
+TARGET_USES_QTI_CAMERA_DEVICE := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND    := true
+BOARD_CHARGER_SHOW_PERCENTAGE   := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += \
+	hardware/lineage/lineagehw \
+	hardware/samsung/lineagehw \
+	$(LOCAL_PATH)/lineagehw
+
+# Display
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+TARGET_HAVE_NEW_GRALLOC := true
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_NEW_ION_API := true
+
+# Encryption
+TARGET_LEGACY_HW_DISK_ENCRYPTION := true
+TARGET_HW_KEYMASTER_V03 := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+ifeq ($(RECOVERY_VARIANT),twrp)
+	TARGET_HW_DISK_ENCRYPTION := false
+	TARGET_SWV8_DISK_ENCRYPTION := false
+else
+	TARGET_HW_DISK_ENCRYPTION := true
+	TARGET_SWV8_DISK_ENCRYPTION := true
+endif
+
+# Filesystems
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE   := ext4
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE    := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# FM
+AUDIO_FEATURE_ENABLED_FM := true
+BOARD_HAVE_QCOM_FM := true
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+
+# GPS
+TARGET_NO_RPC := true
+USE_DEVICE_SPECIFIC_GPS := true
+
+# Healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm8916
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8916
+
+# Kernel
+BOARD_KERNEL_CMDLINE += \
+	console=null \
+	androidboot.hardware=qcom \
+	user_debug=23 \
+	msm_rtb.filter=0x3F \
+	ehci-hcd.park=3 \
+	androidboot.bootdevice=7824900.sdhci \
+	androidboot.selinux=permissive
+
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+LZMA_RAMDISK_TARGETS := recovery
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
+TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
+
+# Kernel - Toolchain
+ifneq ($(wildcard $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin),)
+    KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.2/bin
+    KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+endif
+
+# Malloc implementation
+MALLOC_SVELTE := true
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT           := caf
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# NFC
+BOARD_NFC_HAL_SUFFIX := msm8916
+
+# Partition sizes
+BOARD_BOOTIMAGE_PARTITION_SIZE      := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 15728640
+BOARD_CACHEIMAGE_PARTITION_SIZE     := 314572800
+BOARD_FLASH_BLOCK_SIZE              := 131072
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+CM_POWERHAL_EXTENSION := qcom
+WITH_QC_PERF := true
+
+# Protobuf
+PROTOBUF_SUPPORTED := true
+
+# Qualcomm support
+TARGET_USES_QCOM_BSP := true
+HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE   := true
+USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY := true
+TARGET_USES_NEW_ION_API := true
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/msm8916-common/recovery/recovery_keys.c
+BOARD_HAS_NO_MISC_PARTITION	:= true
+BOARD_HAS_NO_SELECT_BUTTON	:= true
+BOARD_RECOVERY_SWIPE 		:= true
+BOARD_SUPPRESS_EMMC_WIPE	:= true
+BOARD_SUPPRESS_SECURE_ERASE	:= true
+BOARD_USE_CUSTOM_RECOVERY_FONT	:= \"roboto_23x41.h\"
+BOARD_USES_MMCUTILS	:= true
+RECOVERY_GRAPHICS_USE_LINELENGTH	:= true
+RECOVERY_SDCARD_ON_DATA	:= true
+TARGET_RECOVERY_DENSITY	:= hdpi
+TARGET_RECOVERY_FSTAB	:= device/samsung/msm8916-common/recovery/recovery.fstab
+TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
+TARGET_RECOVERY_QCOM_RTC_FIX	:= true
+
+# Recovery - TWRP
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_HAS_DOWNLOAD_MODE := true
+TW_HAS_MTP := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "accelerometer\x0ahbtp_vm"
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_MAX_BRIGHTNESS := 255
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_NEW_ION_HEAP := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_NO_USB_STORAGE := true
+TW_TARGET_USES_QCOM_BSP := false
+TW_THEME := portrait_hdpi
+
+ifeq ($(RECOVERY_VARIANT),twrp)
+	BOARD_GLOBAL_CFLAGS += -DTW_USE_MINUI_CUSTOM_FONTS
+endif
+
+#ifneq ($(wildcard bootable/recovery-twrp),)
+#	RECOVERY_VARIANT := twrp
+#endif
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
+
+#BOARD_SEPOLICY_DIRS += \
+#    $(LOCAL_PATH)/sepolicy
+
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/lib/libmmjpeg_interface.so|libboringssl-compat.so \
+    /system/lib/libsec-ril.so|libshim_secril.so \
+    /system/lib/libsec-ril-dsds.so|libshim_secril.so \
+    /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
+    /system/vendor/lib/libizat_core.so|libshim_gps.so \
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
+
+# Snapdragon LLVM
+TARGET_USE_SDCLANG := true
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Vold
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_VOLD_MAX_PARTITIONS := 67
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+TARGET_USES_QCOM_WCNSS_QMI := true
+TARGET_USES_WCNSS_CTRL := true
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WLAN_CHIPSET := pronto
+WPA_SUPPLICANT_VERSION := VER_0_8_X
