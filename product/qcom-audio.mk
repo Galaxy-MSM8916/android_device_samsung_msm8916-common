8e1c5e84b397f448b8fc908b52773584922dfa91
Wang Han <416810799@qq.com>
2018-09-22 12:41:51 +0800

    msm8916-common: Remove non-existent audio services
     * The audio service rules them all
    
    Change-Id: I97c9864d9c2d93aed8a8f909d34c26fa629ea17e

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -19,6 +19,5 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
