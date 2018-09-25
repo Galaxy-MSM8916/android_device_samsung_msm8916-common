ec796cc15460dadae2b8822706dddefb19dd24a2
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-25 00:35:39 -0400

    msm8916-common: Include sepolicy-legacy repo
     - Legacy policies have been moved to a separate repo.
    
    Change-Id: Ia227092ad8527cef00eec55590cf38fa21189970

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -262,7 +262,7 @@ endif
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+-include device/qcom/sepolicy-legacy/sepolicy.mk
 
 #BOARD_SEPOLICY_DIRS += \
 #    $(LOCAL_PATH)/sepolicy
