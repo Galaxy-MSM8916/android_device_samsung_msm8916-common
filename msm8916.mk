ade8f03302caa238cfd91a90d89843998b1f0c84
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-09-25 20:56:11 -0400

    msm8916-common: Don't build copybit
     - The barrier in display-caf shouldn't allow it to be built
    
    Change-Id: I52baefc8463d973e03eeab328b2d05ddb17c00e1

--- msm8916.mk
+++ msm8916.mk
@@ -151,7 +151,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    copybit.msm8916 \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
     libgenlock \
