ad3ba76048c335a5cbd3e98b56c46901fbf88905
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-10-06 13:41:50 -0400

    [DNM] msm8916-common: Use default libhealthd
    Change-Id: I5d35c7433c091b633a77672e55b0f918329b76fb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,7 +147,7 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
 # Healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
+BOARD_HAL_STATIC_LIBRARIES := libhealthd
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916
