80ee5a0aa1fe8eaea86ab830e305c644e083ff07
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 18:39:15 -0400

    [DNM] msm8916: Don't include sepolicy
    Change-Id: I5bbe2af58bb2460be8c05bca5c56f6236d070e2e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -256,8 +256,8 @@ endif
 #endif
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/legacy-sepolicy.mk
 
 #BOARD_SEPOLICY_DIRS += \
 #    $(LOCAL_PATH)/sepolicy
