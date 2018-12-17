e680e18752ee802f58cc21558ab47b4800e4209d
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    msm8916-common: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- msm8916.mk
+++ msm8916.mk
@@ -216,6 +216,10 @@ PRODUCT_PACKAGES += \
     memtrack.msm8916 \
     vendor.lineage.livedisplay@2.0-service.samsung-qcom
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
