ada9ea2769e7fda0384038523d067f1428c6f7f7
Paul McLean <pmclean@google.com>
2019-10-05 15:03:09 +0100

    msm8916-common: Adding Audio HAL V5: Direction API
    Change-Id: I66359e5009e68a78cc1afd5060948df512b3b2ac

--- msm8916.mk
+++ msm8916.mk
@@ -37,9 +37,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@5.0 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio.primary.default \
