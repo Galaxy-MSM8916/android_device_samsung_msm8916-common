d5875e30dcf6fd9747e8ab4526c71c110441330f
Wang Han <416810799@qq.com>
2019-10-22 14:18:55 +0800

    msm8916: Remove deprecated ipv6 tethering support
     * This is not a thing anymore since N.
    
    Change-Id: I944115cc8a33bd3ea21cb9c64c9c98a97d8f0373
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- msm8916.mk
+++ msm8916.mk
@@ -168,12 +168,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
