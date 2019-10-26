f8e33d588c53f68d0f820cc817ee952e409cb7ab
rajadeja <raj.jdja@gmail.com>
2019-10-26 07:33:35 +0000

    msm8916-common: Add authsecret
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -271,6 +271,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.authsecret@1.0-service \
     keystore.msm8916
 
 # Lights
