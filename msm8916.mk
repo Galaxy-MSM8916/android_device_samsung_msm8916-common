4524bbee538937308aafe9861116a19c4dd4de4c
Nicholas Lim <nicknitewolf@lineageos.org>
2019-10-29 18:01:57 +0800

    msm8916-common: Exclude AudioFX
    Change-Id: I832397f98f9860c5074d2a5a6235d62b9d398d04

--- msm8916.mk
+++ msm8916.mk
@@ -196,6 +196,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # FM
 PRODUCT_PACKAGES += \
     qcom.fmradio \
