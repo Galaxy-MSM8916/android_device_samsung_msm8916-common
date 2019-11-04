dbdfeec47bd4cfb171abe5dcfd1160d652973d19
Soft-Bullet <binaytiwari6@gmail.com>
2019-11-04 11:19:43 +0545

    msm8916-common: Drop mediaextractor seccomp policy

--- msm8916.mk
+++ msm8916.mk
@@ -416,7 +416,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
