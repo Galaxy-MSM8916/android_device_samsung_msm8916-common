c9190a14be5ba514bd7bd001e49c00796ad6ce1e
Jens Lody <jens@jenslody.de>
2019-11-21 22:34:45 +0100

    msm8916-common: fix dependencies of CameraWrapper
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I20326eb5b7f3b8f33307e7ff2ce983f2ad9b7c0c

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,13 +9,15 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_HEADER_LIBRARIES += \
-    libnativebase_headers
+    libnativebase_headers \
+    libnativewindow_headers
 
 LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils libcutils libdl \
     android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    android.hardware.graphics.bufferqueue@1.0 \
+    android.hardware.graphics.bufferqueue@2.0
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
