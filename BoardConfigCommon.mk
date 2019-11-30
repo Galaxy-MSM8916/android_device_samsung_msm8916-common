e740e7e3f3c8657c0a7434f6d3186bcdce86a4c7
Jens Lody <jens@jenslody.de>
2019-11-30 09:51:05 +0100

    msm8916-common: Use libbase_shim to avoid bluetooth-crash.
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -267,7 +267,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
     /system/vendor/lib/libizat_core.so|libshim_gps.so \
-    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so
+    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /system/vendor/lib/hw/android.hardware.bluetooth@1.0-impl-qti.so|libbase_shim.so
 
 # Snapdragon LLVM
 TARGET_USE_SDCLANG := true
