57a5044be9413841ed8d1b0fb8786794a303bc57
Jens Lody <jens@jenslody.de>
2019-12-01 20:29:00 +0100

    msm8916-common: Add power.stats
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- msm8916.mk
+++ msm8916.mk
@@ -381,6 +381,7 @@ PRODUCT_COPY_FILES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti \
+    android.hardware.power.stats@1.0-service \
     power.qcom
 
 # Radio
