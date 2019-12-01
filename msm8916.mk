0b1ca5779d7e35e65fa7df39ff2a90dda23c27e3
Jens Lody <jens@jenslody.de>
2019-12-01 20:48:02 +0100

    msm8916-common: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4
    Signed-off-by: Jens Lody <jens@jenslody.de>
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- msm8916.mk
+++ msm8916.mk
@@ -364,8 +364,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
+    android.hardware.power@1.2-service-qti \
     power.qcom
 
 # Radio
