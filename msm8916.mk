cba4936a2b486063246a2a14bdd60403f7feef16
DarkDroidDev <darkdroid.dev@gmail.com>
2020-02-16 10:34:31 +0100

    msm8916-common: Build DRM clearkey 1.2
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: Ic3db2b7ff171d0692e13ae660552e7baf61c1a3a

--- msm8916.mk
+++ msm8916.mk
@@ -190,7 +190,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
 
 # Encryption
 PRODUCT_PACKAGES += \
