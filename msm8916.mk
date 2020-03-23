e1f88f77407ded34cbdacd77ebfaac2538f2bdf2
Bruno Martins <bgcngm@gmail.com>
2020-03-23 12:16:29 +0100

    msm8916-common: Convert lights HAL into a native binderized HAL
    Change-Id: Ie729c67a71d59ea004255f454b41918bf41716d7

--- msm8916.mk
+++ msm8916.mk
@@ -334,8 +334,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8916
+    android.hardware.light@2.0-service.msm8916
 
 # Media configurations
 PRODUCT_COPY_FILES += \
