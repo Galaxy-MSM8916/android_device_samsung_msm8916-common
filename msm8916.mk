07906f751042b1223db43a82e4ea7b0759e2c7d8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-24 10:33:32 +0100

    msm8916-common: readd custom Dalvik heap values

--- msm8916.mk
+++ msm8916.mk
@@ -109,6 +109,14 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
 # Dalvik Heap
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Data configuration files
