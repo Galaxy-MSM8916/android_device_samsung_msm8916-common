6341fff3dd2933993184d352b43df4687d6d7bf1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-25 21:20:55 +0100

    msm8916-common: build soundtrigger 2.2 HIDL

--- msm8916.mk
+++ msm8916.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio.effect@5.0 \
-    android.hardware.audio.effect@5.0-impl
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
