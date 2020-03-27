264a05ac1b984ed1d042331ce19482041e468cc8
Jens Lody <jens@jenslody.de>
2020-03-27 23:14:43 +0100

    msm8916-common: split power_profile.xml overlay
    2015 and 2016 models have different batttery capacity
    
    Change-Id: I0bc4f1f50b122baaa66a1ac897865faf9de9b6fe

--- device-j5.mk
+++ device-j5.mk
@@ -1,2 +1,5 @@
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5-common/j5-common-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay_j5
--- device-j5x.mk
+++ device-j5x.mk
@@ -1,2 +1,5 @@
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5x-common/j5x-common-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay_j5x
