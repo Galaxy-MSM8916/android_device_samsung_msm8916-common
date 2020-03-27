cbeb11e249bc0d3f0f151410d464b0c0abf5b12d
Ataberk Özen <ataberkozen123@gmail.com>
2020-03-27 20:38:57 +0100

    msm8916-common: build NFC HAL 1.1
    Change-Id: I5a423b4a0461dad37c47b4fc4075b194cb07b7fb

--- msm8916.mk
+++ msm8916.mk
@@ -264,6 +264,21 @@ PRODUCT_PACKAGES += \
 
 # NFC - only for j5nlte and j5xnlte
 ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),j5nlte j5xnlte))
+PRODUCT_PACKAGES += \
+	android.hardware.nfc@1.1 \
+	android.hardware.nfc@1.1-service \
+    vendor.nxp.nxpnfc@1.0 \
+    vendor.nxp.hardware.nfc@1.0-service
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    nfc_nci.nqx.default \
+    nqnfcee_access.xml \
+    NfcNci \
+    Tag
+
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
 	$(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
