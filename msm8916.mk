8eb441c196d000981ba3579aa5c543947c034840
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-27 20:38:19 +0100

    msm8916-common: add NFC permissions

--- msm8916.mk
+++ msm8916.mk
@@ -269,6 +269,11 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/libnfc-nxp.conf
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+
 DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/manifest-nfc.xml
 endif
 
