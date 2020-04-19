11ac4f8178415f37f28d7bc114ea975d298c3d43
Jens Lody <jens@jenslody.de>
2020-04-19 15:27:42 +0200

    msm8916-common: Don't build keystore.
    Fixes locksettings not working.
    
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- msm8916.mk
+++ msm8916.mk
@@ -236,8 +236,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service \
-    android.hardware.authsecret@1.0-service \
-    keystore.msm8916
+    android.hardware.authsecret@1.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
