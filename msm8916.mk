067ed47b4383afa051458483e2737e51ad74b947
Ethan Chen <intervigil@gmail.com>
2020-04-20 23:22:17 +0200

    Revert "msm8916-common: Add basic USB HAL that reports no status change"
    This reverts commit 2d258bfabc23762aab3c75d189ddacdf3907024e.

--- msm8916.mk
+++ msm8916.mk
@@ -372,7 +372,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.msm8916
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
