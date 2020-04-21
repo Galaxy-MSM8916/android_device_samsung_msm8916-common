21a2aef5d1a4dcfc5152ebb44bb0af36051d35d9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-21 09:13:55 +0200

    msm8916-common: remove batter capacity overlay
    * Android is able to pick this value from kernel

--- device-j5.mk
+++ device-j5.mk
@@ -15,6 +15,3 @@
 
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5-common/j5-common-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-j5
--- device-j5x.mk
+++ device-j5x.mk
@@ -15,6 +15,3 @@
 
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5x-common/j5x-common-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-j5x
