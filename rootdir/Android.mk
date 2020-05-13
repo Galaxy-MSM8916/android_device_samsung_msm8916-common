398c2de7fe09af609110e9074d4b6e8f64b47d1a
zqm64 <zqm64@outlook.com>
2020-05-13 17:34:40 +0800

    msm8916-common: rootdir: Remove the eng tag in init.target.rc

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -144,7 +144,7 @@ include $(BUILD_PREBUILT)
 ### init.target.rc (for the MSM8929 SoC)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
