559f105512aebdc57162864c6c0f0d6e5d685e61
LuK1337 <priv.luk@gmail.com>
2020-05-15 11:06:35 +0200

    msm8916-common: remove camera.device@1.0-impl from PRODUCT_PACKAGES
    * camera.device@1.0-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- msm8916.mk
+++ msm8916.mk
@@ -94,8 +94,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    android.hardware.camera.provider@2.4-impl
 
 PRODUCT_PACKAGES += \
     camera.msm8916 \
