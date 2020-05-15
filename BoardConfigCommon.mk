121feadaa4c1bdb945281bba2ffee4d3091bdcd3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-05-15 09:09:10 +0200

    msm8916-common: drop TWRP support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,40 +200,7 @@ HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
 TARGET_USES_NEW_ION_API := true
 
 # Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/msm8916-common/recovery/recovery_keys.c
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_USES_MMCUTILS := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA	:= true
-TARGET_RECOVERY_DENSITY	:= hdpi
-TARGET_RECOVERY_FSTAB := device/samsung/msm8916-common/recovery/recovery.fstab
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TARGET_RECOVERY_QCOM_RTC_FIX	:= true
-
-# Recovery - TWRP
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_HAS_DOWNLOAD_MODE := true
-TW_HAS_MTP := true
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_NTFS_3G := true
-TW_INPUT_BLACKLIST := "accelerometer\x0ahbtp_vm"
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_MAX_BRIGHTNESS := 255
-TW_MTP_DEVICE := /dev/mtp_usb
-TW_NEW_ION_HEAP := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_NO_USB_STORAGE := true
-TW_TARGET_USES_QCOM_BSP := false
-TW_THEME := portrait_hdpi
-
-ifeq ($(RECOVERY_VARIANT),twrp)
-	BOARD_GLOBAL_CFLAGS += -DTW_USE_MINUI_CUSTOM_FONTS
-endif
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
