67bece4b89ffd8c9f5e2f2d84b45e8758615b9e5
itexpert120 <itexpert120@gmail.com>
2020-08-18 06:33:30 +0200

    Changes for RR

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -560,3 +560,42 @@ case "$target" in
         echo $oem_version > /sys/devices/soc0/image_crm_version
         ;;
 esac
+
+# Enable Limiter and Thermel
+echo "1" > /sys/kernel/msm_limiter/limiter_enabled
+echo "1" > /sys/kernel/msm_thermal/enabled
+
+# fix permissions
+chmod 644 /sys/devices/system/cpu/cpu0/online
+chmod 644 /sys/devices/system/cpu/cpu1/online
+chmod 644 /sys/devices/system/cpu/cpu2/online
+chmod 644 /sys/devices/system/cpu/cpu3/online
+
+# Set Default Governor and Values
+echo "blu_active" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "blu_active" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "blu_active" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "blu_active" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+echo "19000 1248000:29000" > /sys/devices/system/cpu/cpufreq/blu_active/above_hispeed_delay
+echo "85" > /sys/devices/system/cpu/cpufreq/blu_active/go_hispeed_load
+echo "1248000" > /sys/devices/system/cpu/cpufreq/blu_active/hispeed_freq
+echo "85 1248000:80" > /sys/devices/system/cpu/cpufreq/blu_active/target_loads
+echo "30000" > /sys/devices/system/cpu/cpufreq/blu_active/timer_rate
+echo "60000" > /sys/devices/system/cpu/cpufreq/blu_active/timer_slack
+
+#Fix suddenly reboot issue
+
+echo 1 > /sys/bus/cpu/devices/cpu0/core_ctl/min_cpus
+echo "0 0 0 0" >/sys/bus/cpu/devices/cpu0/core_ctl/busy_down_thres
+echo "0 0 0 0" >/sys/bus/cpu/devices/cpu0/core_ctl/busy_up_thres
+
+sleep 3
+
+echo "80 80 80 80" >/sys/bus/cpu/devices/cpu0/core_ctl/busy_up_thres
+
+#cpu_freq
+
+sleep 5
+echo 1612000 >/sys/bus/cpu/devices/cpu0/cpufreq/scaling_max_freq
+echo 200000 >/sys/bus/cpu/devices/cpu0/cpufreq/scaling_min_freq
+
