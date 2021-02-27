68279d528a7317aeaaf925ca8fd8e93642d1d0a9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-27 12:16:23 +0100

    msm8916-common: Include android.hidl.* in HIDL section
    Change-Id: I89e70a690f4155dd1db8e7da8bef0af9fe9aea74

--- msm8916.mk
+++ msm8916.mk
@@ -178,12 +178,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
-# HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
