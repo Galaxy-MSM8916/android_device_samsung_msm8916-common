61b0a9069b7b7e106d24d368a318426941e7bc47
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-27 12:14:43 +0100

    msm8916-common: Cleanup audio packages
    * They are all a dependency of android.hardware.audio@6.0-impl and android.hardware.audio.effect@6.0-impl
    
    Change-Id: I888249956b9d1658ff780d0849eb35cf6925cd2a

--- msm8916.mk
+++ msm8916.mk
@@ -35,14 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@6.0 \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.common@6.0 \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
