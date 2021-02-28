fd914eaeea9515905e9b37fc06b3e43c2aa7fe0a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-28 23:14:29 +0100

    msm8916-common: Build standard camera provider impl
    * Camera provider impl got split into legacy and external, let's build both
    
    Change-Id: Ic15fd724b19b32796dece08fb2b66fc9f51ae643

--- msm8916.mk
+++ msm8916.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libcamera_shim \
     camera.msm8916 \
