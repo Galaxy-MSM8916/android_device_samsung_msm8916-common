26f7c229433277f2ea4f80e179a73ffdd44f4e33
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-28 16:15:22 +0100

    msm8916-common: Binderize keymaster HIDL
    Change-Id: I30cad42ba0bd2c36594ab8fd1093aab2ba0b28ef

--- msm8916.mk
+++ msm8916.mk
@@ -192,6 +192,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     keystore.msm8916
 
 # Lights
