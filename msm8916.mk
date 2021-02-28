ec6bfb80869eedb1793f9cd2a558ee4053af789b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-28 23:12:55 +0100

    msm8916-common: Binderize camera provider
    Change-Id: I40b523dc2b23a56d0ee229717dc53a46068312b4

--- msm8916.mk
+++ msm8916.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-service \
     libcamera_shim \
     camera.msm8916 \
     Snap
