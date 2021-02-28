6ad1c5e009170daffeba30fc7e1a8aa74ec6dc89
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-28 19:42:21 +0100

    msm8916-common: Drop soundtrigger HIDL
    * soundtrigger requires a vendor HAL and Google HotwordEnrollment anyway
    
    Change-Id: I0e0bd45dca23dfa93c6d18e04b60d3129a95d609

--- msm8916.mk
+++ msm8916.mk
@@ -38,7 +38,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8916 \
