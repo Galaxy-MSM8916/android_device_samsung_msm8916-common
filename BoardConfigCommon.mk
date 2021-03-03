d3610643b4478b7f5a66b0758eb50897ce8df2ef
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-03 08:56:22 +0100

    msm8916-common: Replace /system/vendor with /vendor
    Change-Id: I2d28e8fb5cb91e41fe30ad5d09122ff80e31bf1c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,10 +184,10 @@ TARGET_LD_SHIM_LIBS := \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
-    /system/vendor/lib/libizat_core.so|libshim_gps.so \
-    /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
-    /system/vendor/lib/hw/android.hardware.bluetooth@1.0-impl-qti.so|libbase_shim.so \
-    /system/vendor/lib/libgeofence.so|liblocadapterbase_shim.so
+    /vendor/lib/libizat_core.so|libshim_gps.so \
+    /vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
+    /vendor/lib/hw/android.hardware.bluetooth@1.0-impl-qti.so|libbase_shim.so \
+    /vendor/lib/libgeofence.so|liblocadapterbase_shim.so
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
