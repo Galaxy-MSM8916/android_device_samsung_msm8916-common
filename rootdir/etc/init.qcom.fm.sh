7bc65ca9b8226d1f165235b230b8c3408a124d39
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-03 08:56:22 +0100

    msm8916-common: Replace /system/vendor with /vendor
    Change-Id: I2d28e8fb5cb91e41fe30ad5d09122ff80e31bf1c

--- rootdir/etc/init.qcom.fm.sh
+++ rootdir/etc/init.qcom.fm.sh
@@ -60,17 +60,17 @@ case $mode in
   "normal")
         logi "inserting the radio transport module"
         echo 1 > /sys/module/radio_iris_transport/parameters/fmsmd_set
-        /system/vendor/bin/fm_qsoc_patches $version 0
+        /vendor/bin/fm_qsoc_patches $version 0
      ;;
   "wa_enable")
-   /system/vendor/bin/fm_qsoc_patches $version 1
+   /vendor/bin/fm_qsoc_patches $version 1
      ;;
   "wa_disable")
-   /system/vendor/bin/fm_qsoc_patches $version 2
+   /vendor/bin/fm_qsoc_patches $version 2
      ;;
    *)
     logi "Shell: Default case"
-    /system/vendor/bin/fm_qsoc_patches $version 0
+    /vendor/bin/fm_qsoc_patches $version 0
     ;;
 esac
 
